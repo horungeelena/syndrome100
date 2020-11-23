91c54d90f57d10baddbcb36551c01c267c51ae61
Elena Fedoseeva <horungeelena@gmail.com>
2020-11-23 17:58:30 +0200

    find the maximum estimated annual income

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -6,27 +6,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StatisticsServiceTest {
 
-  @Test
-  void findMax() {
-    StatisticsService service = new StatisticsService();
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
 
-    long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-    long expected = 12;
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
 
-    long actual = service.findMax(incomesInBillions);
+        long actual = service.findMax(incomesInBillions);
 
-    assertEquals(expected, actual);
-  }
+        assertEquals(expected, actual);
+    }
+    @Test
+    void findMaxEstimatedAnnualIncomes() {
+        StatisticsService service = new StatisticsService();
 
-  @Test
-  void findMin() {
-    StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12, 16};
+        long expected = 16;
 
-    long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-    long expected = 3;
+        long actual = service.findMax(incomesInBillions);
 
-    long actual = service.findMin(incomesInBillions);
-
-    assertEquals(expected, actual);
-  }
+        assertEquals(expected, actual);
+    }
 }
